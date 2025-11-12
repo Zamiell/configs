ab13ea10f868352684d993170731815acb3aa1bf
sillydodo <32281134+Jeenil@users.noreply.github.com>
2025-11-12 14:49:18 -0500

    feat(bash): add alias for dev package script (#2)

--- ubuntu-auto-install/post-install/post-install.sh
+++ ubuntu-auto-install/post-install/post-install.sh
@@ -6,12 +6,12 @@ set -euo pipefail # Exit on errors and undefined variables.
 
 # Get the directory of this script:
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
 # Perform validation.
 if [[ $EUID -ne 0 ]]; then
- echo "This script must be run as root" >&2
- exit 1
+  echo "This script must be run as root" >&2
+  exit 1
 fi
 
 # Connect to WiFi.
