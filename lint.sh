824ef55e281bb31d7d4f71ac85c00467aad1dbab
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-12-05 12:44:48 -0500

    chore: standardize DIR

--- lint.sh
+++ lint.sh
@@ -4,11 +4,11 @@ set -euo pipefail # Exit on errors and undefined variables.
 
 # Get the directory of this script:
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 # Get the name of this repository:
 # https://stackoverflow.com/questions/23162299/how-to-get-the-last-part-of-dirname-in-bash/23162553
-REPO="$(basename "$DIR")"
+REPO=$(basename "$DIR")
 
 cd "$DIR"
 
