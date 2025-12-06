2d16bfd29e7602c6d907e32895245d6b99d08563
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-12-06 09:11:14 -0500

    chore: update shellcheck invocation

--- lint.sh
+++ lint.sh
@@ -13,7 +13,7 @@ REPO=$(basename "$DIR")
 cd "$DIR"
 
 bunx prettier --log-level=warn --check .
-find . -name "node_modules" -prune -o -type f -name "*.sh" -exec shellcheck {} +
+find . \( -name "node_modules" -o -name ".venv" \) -prune -o -type f -name "*.sh" -exec shellcheck {} +
 shellcheck ./bash/.bash_profile_remote # The main config file does not have a ".sh" extension.
 bunx cspell --no-progress --no-summary
 bunx cspell-check-unused-words
