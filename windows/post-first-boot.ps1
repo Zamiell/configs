fd9002263c981017ac7954d8879934142e834c27
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-12-23 00:39:11 -0500

    fix

--- windows/post-first-boot.ps1
+++ windows/post-first-boot.ps1
@@ -1,5 +1,6 @@
 #Requires -RunAsAdministrator
 
+$scriptsPath = "C:\Windows\Setup\scripts"
 $scriptName = "windows-update.ps1"
 $scriptPath = "$scriptsPath\$scriptName"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Zamiell/configs/refs/heads/main/windows/$scriptName" -OutFile $scriptPath
