a7ffb003760874a200f079a9374cecb938f39e8a
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-12-23 09:22:01 -0500

    doc: snap

--- windows/set-windows-settings.ps1
+++ windows/set-windows-settings.ps1
@@ -23,6 +23,7 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v En
 
 # Settings --> System --> Multitasking --> Snap windows --> Uncheck "Show snap layouts when I drag a window to the top of the screen"
 # https://www.elevenforum.com/t/enable-or-disable-snap-layouts-on-top-of-screen-in-windows-11.6142/
+# - This requires a restart of explorer to take effect.
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v EnableSnapBar /t REG_DWORD /d 0 /f
 
 # --------------------------------
