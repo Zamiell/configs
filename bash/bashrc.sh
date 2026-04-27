e9a74f610189acc876f6e712849defc07e8658b7
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-04-27 08:14:25 -0400

    fix: directory name in prompt

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -936,14 +936,14 @@ if [[ $- == *i* ]]; then
   if is-ubuntu || is-mac-os; then
     # We copy the prompt from Git Bash for Windows:
     # https://github.com/git-for-windows/build-extra/blob/main/git-extra/git-prompt.sh
-    PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
-    PS1="$PS1"'\n'                         # new line
-    PS1="$PS1"'\[\033[32m\]'               # change to green
-    PS1="$PS1"'\u@\h '                     # user@host<space>
-    PS1="$PS1"'\[\033[35m\]'               # change to purple
-    PS1="$PS1"'$MSYSTEM '                  # show MSYSTEM
-    PS1="$PS1"'\[\033[33m\]'               # change to brownish yellow
-    PS1="$PS1"'\w'                         # current working directory
+    PS1='\[\033]0;\W\007\]'  # set window title (modified to only show directory name)
+    PS1="$PS1"'\n'           # new line
+    PS1="$PS1"'\[\033[32m\]' # change to green
+    PS1="$PS1"'\u@\h '       # user@host<space>
+    PS1="$PS1"'\[\033[35m\]' # change to purple
+    PS1="$PS1"'$MSYSTEM '    # show MSYSTEM
+    PS1="$PS1"'\[\033[33m\]' # change to brownish yellow
+    PS1="$PS1"'\w'           # current working directory
     GIT_EXEC_PATH="$(git --exec-path 2> /dev/null)"
     COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
     COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
