60935a2eb43373fdcc85b3d05be0230c1bc2e1a9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-04-30 08:38:39 -0400

    feat: terraform

--- bash/setup-wsl.sh
+++ bash/setup-wsl.sh
@@ -118,6 +118,18 @@ if ! command -v uv &> /dev/null; then
   curl --silent --fail --show-error --location https://astral.sh/uv/install.sh | sh
 fi
 
+# Install Terraform.
+# https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
+if ! command -v terraform &> /dev/null; then
+  curl --silent --fail --show-error --location https://apt.releases.hashicorp.com/gpg \
+    | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
+    | sudo tee /etc/apt/sources.list.d/hashicorp.list
+  sudo apt-get update
+  sudo apt-get install terraform --yes
+fi
+
 # --------------------------------
 # Install quality of life software
 # --------------------------------
