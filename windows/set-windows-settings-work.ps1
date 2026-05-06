5288ad9c4850c125a1e1d5b5d2d2fafa782f1416
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-05-06 18:55:07 -0400

    fix: yubikey

--- windows/set-windows-settings-work.ps1
+++ windows/set-windows-settings-work.ps1
@@ -5,7 +5,7 @@
 # https://www.elevenforum.com/t/enable-or-disable-fast-user-switching-in-windows-11.4620/
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "HideFastUserSwitching" /t REG_DWORD /d 1 /f
 
-# Pin the default credential provider to be Windows Hello (to stop it from trying to use a YubiKey).
-# The magic string is the GUID for the Windows Hello PIN.
-# https://learn.microsoft.com/en-us/windows/client-management/mdm/policy-csp-admx-credentialproviders#defaultcredentialprovider
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "DefaultCredentialProvider" /t REG_SZ /d "{D6886603-9D2F-4EB2-B667-1971041FA96B}" /f
+# Banish YubiKey from the login screen.
+# https://learn.microsoft.com/en-us/answers/questions/1302097/how-to-configure-windows-10-to-show-only-smart-car
+# TODO: Does this work?
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ExcludeCredentialProviders" /t REG_SZ /d "{8FD7E19C-3BF7-489B-A72C-846AB3678C96}" /f
