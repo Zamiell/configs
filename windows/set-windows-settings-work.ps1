5cb0b57a12daf15a9b827718048c19cd9db6d1df
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-05-10 07:17:12 -0400

    feat!: remove yubikey

--- windows/set-windows-settings-work.ps1
+++ windows/set-windows-settings-work.ps1
@@ -5,12 +5,6 @@
 # https://www.elevenforum.com/t/enable-or-disable-fast-user-switching-in-windows-11.4620/
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "HideFastUserSwitching" /t REG_DWORD /d 1 /f
 
-# Banish YubiKey from the login screen.
-# https://learn.microsoft.com/en-us/answers/questions/1302097/how-to-configure-windows-10-to-show-only-smart-car
-# TODO: Does not work.
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "ExcludedCredentialProviders" /t REG_SZ /d "{8FD7E19C-3BF7-489B-A72C-846AB3678C96},{F8A0B131-5F68-486C-8040-7E8FC3C85BB6}" /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "ExcludedCredentialProviders" /t REG_SZ /d "{8FD7E19C-3BF7-489B-A72C-846AB3678C96},{F8A0B131-5F68-486C-8040-7E8FC3C85BB6}" /f
-
 # Internet Options --> Security --> Local intranet --> Sites --> Advanced -->
 # azuredevops.logixhealth.com --> Add
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\logixhealth.com\azuredevops" /v https /t REG_DWORD /d 1 /f
