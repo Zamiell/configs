7b4433947fa39a982aadb0ac4de829279d283196
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-06-19 20:40:55 -0400

    feat: copy.sh

---
+++ copilot/copy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail # Exit on errors and undefined variables.
+
+# Get the directory of this script:
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+cp "$DIR/settings.json" "$HOME/.copilot/settings.json"
