5124d39262dadfbc77903e2f1bb56ceb0682a63c
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-08-19 16:01:49 -0400

    feat: copy.sh messages

--- app-settings/zed/copy.sh
+++ app-settings/zed/copy.sh
@@ -6,4 +6,12 @@ set -euo pipefail # Exit on errors and undefined variables.
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-cp "$DIR/settings.json" "/mnt/c/Users/$USER/AppData/Roaming/Zed/settings.json"
+SETTINGS_PATH="/mnt/c/Users/$USER/AppData/Roaming/Zed/settings.json"
+
+if [[ -f "$SETTINGS_PATH" ]] && cmp --silent "$DIR/settings.json" "$SETTINGS_PATH"; then
+  echo "The Zed settings are already up to date. Nothing needs to be updated."
+  exit 0
+fi
+
+cp "$DIR/settings.json" "$SETTINGS_PATH"
+echo "Successfully updated: $SETTINGS_PATH"
