e91b53b7ec3d291919821ef808468af161cfb550
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-09-07 11:21:18 -0400

    fix: gprh for worktrees

--- bash/commands/git.sh
+++ bash/commands/git.sh
@@ -1089,12 +1089,23 @@ gprh() (
   assert-in-git-repository
   assert-feature-branch
 
-  if [[ -z "${REPOSITORIES_DIR:-}" ]]; then
-    echo "Error: You can only use this command if your repositories directory is in one of the standard locations." >&2
-    exit 1
+  local infrastructure_path
+  local current_repo_root
+  local current_repo_remote_url
+  if current_repo_root=$(git rev-parse --show-toplevel 2> /dev/null) \
+    && current_repo_remote_url=$(git -C "$current_repo_root" remote get-url origin 2> /dev/null) \
+    && [[ "${current_repo_remote_url%.git}" == */infrastructure ]]; then
+    infrastructure_path="$current_repo_root"
+  else
+    if [[ -z "${REPOSITORIES_DIR:-}" ]]; then
+      echo "Error: You can only use this command if your repositories directory is in one of the standard locations." >&2
+      exit 1
+    fi
+
+    infrastructure_path="$REPOSITORIES_DIR/infrastructure"
   fi
 
-  local scripts_path="$REPOSITORIES_DIR/infrastructure/0-global-library/typescript-scripts"
+  local scripts_path="$infrastructure_path/0-global-library/typescript-scripts"
   if [[ ! -d "$scripts_path" ]]; then
     echo "Error: The directory does not exist at: $scripts_path" >&2
     exit 1
